eb2410ed154c41f73fd7b260d00c8caef1ce6403
angelemmavelasco <angel.emma.velasco@gmail.com>
2026-06-05 10:55:19 -0600

    fix debug variable

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     'localhost',
