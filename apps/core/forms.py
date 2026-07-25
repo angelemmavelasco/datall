ceca6a786158f2f9880fcd5a9f5eef802919aec3
angelemmavelasco <angel.emma.velasco@gmail.com>
2026-07-25 13:54:39 -0600

    feat: implement role-based user management service and form field access control

--- apps/core/forms.py
+++ apps/core/forms.py
@@ -5,4 +5,20 @@ class UserForm(ModelForm):
     class Meta:
         model = User
         fields = '__all__'
-        exclude = ('user_permissions',)
+        exclude = ('password', 'user_permissions', 'last_login', 'date_joined')
+
+    def __init__(self, *args, requesting_user=None, is_full_access=False, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if not requesting_user:
+            return
+
+        is_superuser = getattr(requesting_user, 'is_superuser', False)
+        if not is_superuser:
+            self.fields.pop('is_superuser', None)
+
+        if not is_full_access and not is_superuser:
+            disallowed_fields = ['is_staff', 'is_active', 'groups', 'username',]
+            for field_name in disallowed_fields:
+                self.fields.pop(field_name, None)
+        
--- apps/core/services/users.py
+++ apps/core/services/users.py
@@ -1,16 +1,27 @@
 from django.contrib.auth import get_user_model
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 from django.db.models import QuerySet
+from django.db import transaction
 
+#exceptions
+class ServiceError(Exception):
+    pass
+
+class UserNotFoundError(ServiceError):
+    pass
+
+class UserPermissionError(ServiceError):
+    pass
+
+class UserAuthenticationError(ServiceError):
+    pass
 
 
 if TYPE_CHECKING:
     from django.contrib.auth.models import AbstractBaseUser as UserModel
 else:
     UserModel = object
-
-User = get_user_model()
 
 @dataclass
 class UsersService:
@@ -19,15 +30,101 @@ class UsersService:
     This service handles the business logic of the users module.
     '''
     user: 'UserModel'
+    User: type['UserModel'] = get_user_model()
     _is_full_access: bool = field(init=False)
 
     def __post_init__(self) -> bool:
+        self._validate_access()
         self._is_full_access = self._checkout_full_access
+
+        
+    def _validate_access(self) -> None:
+        '''
+        validates if the user was provided, exists and is authenticated.
+        '''
+        if not self.user:
+            raise UserNotFoundError('No se ha proporcionado un usuario válido.')
+        if not self.user.is_authenticated:
+            raise UserAuthenticationError('El usuario proporcionado no está autenticado.')
+        if not getattr(self.user, 'is_active', True):
+            raise UserPermissionError('El usuario se encuentra inactivo.')
 
     @property
     def _checkout_full_access(self) -> bool:
         '''
         validates if user has total access (or is superuser) or limited access.
         '''
-        return None 
-        
+        if getattr(self.user, 'is_superuser', False):
+            return True
+        return self.user.groups.filter(name__in=['total', 'acceso total', 'admin', 'global', 'acceso global']).exists()
+
+    def read_users(self) -> QuerySet:
+        '''
+        returns a qs which the main user has access to.
+        '''
+        if self._is_full_access:
+            return self.User.objects.all()
+        else:
+            return self.User.objects.filter(pk=self.user.pk)
+    
+    def read_user(self, *, pk: int) -> Optional['UserModel']:
+        '''
+        return a single object depending on the user's permissions.
+        only allowed users and groups can access to any pk user. Otherwise only return self.user.
+        '''
+        return self.read_users().filter(pk=pk).first()
+    
+    def create_user(self, **data) -> 'UserModel':
+        '''
+        create a new user based on provided data.
+        '''
+        if not self._is_full_access:
+            raise UserPermissionError('El usuario no tiene permisos para crear usuarios.')
+        password = data.pop('password', None)
+        groups = data.pop('groups', [])
+        with transaction.atomic():
+            new_user = self.User(**data)
+            if password:
+                new_user.set_password(password)
+            else:
+                new_user.set_unusable_password()
+            new_user.save()
+            if groups:
+                new_user.groups.set(groups)
+        return new_user
+    
+    def update_user(self, *, pk:int, **new_data) -> 'UserModel':
+        '''
+        update a user based on the provided data.
+
+        - for users who are full access or superusers can update any user (except by the password and superuser status).
+        - for regular users, they can view and modified all their info (except by roles, groups, and username)
+        '''
+        user_to_update = self.read_user(pk=pk)
+        if user_to_update is None:
+            raise UserNotFoundError(f'No se encontro el usuario con id {pk}.')
+
+        if getattr(self.user, 'is_superuser', False):
+            disallowed = ['password', 'id', 'pk', 'user_permissions']
+        elif self._is_full_access:
+            disallowed = ['is_superuser', 'is_staff', 'password']
+        elif self._is_full_access:
+            disallowed = ['id', 'pk', 'password', 'is_superuser']
+        else:
+            disallowed = [
+                'id', 'pk', 'password', 'is_superuser', 'is_staff', 
+                'is_active', 'groups', 'user_permissions', 'username', 
+                'last_login', 'date_joined'
+            ]
+
+        for key in disallowed: #remove disallowed fields to prevent errors
+            new_data.pop(key, None)
+        groups = new_data.pop('groups', None)
+
+        with transaction.atomic():
+            for attr, value in new_data.items(): #update simple attrs
+                setattr(user_to_update, attr, value)
+            user_to_update.save()
+            if groups is not None: #update groups if were allowed
+                user_to_update.groups.set(groups)
+        return user_to_update
