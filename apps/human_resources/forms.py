63ad86a344612370619a83c42a34b5434797379f
angelemmavelasco <angel.emma.velasco@gmail.com>
2026-08-13 14:28:49 -0600

    feat: add custom MonitoringFormQuestionForm with filtered queryset and update monitoring form template UI

--- apps/human_resources/forms.py
+++ apps/human_resources/forms.py
@@ -42,9 +42,19 @@ class MonitoringFormScheduleForm(forms.ModelForm):
         model = MonitoringFormSchedule
         fields = ['open_day', 'week_of_month', 'open_time', 'duration_hours']
 
+class MonitoringFormQuestionForm(forms.ModelForm):
+    class Meta:
+        model = MonitoringFormQuestion
+        fields = ['question', 'hierarchy_level', 'position', 'order', 'is_required']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'question' in self.fields:
+            self.fields['question'].queryset = MonitoringFormField.objects.filter(is_active=True)
+
 MonitoringFormQuestionFormSet = inlineformset_factory(
     MonitoringForm, MonitoringFormQuestion,
-    fields=['question', 'hierarchy_level', 'position', 'order', 'is_required'],
+    form=MonitoringFormQuestionForm,
     extra=1,
     can_delete=True
 )
